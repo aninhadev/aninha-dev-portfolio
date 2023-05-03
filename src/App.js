e4d61246e944ce34d7534ffc94da55b062728c43
Ana Maria <anamarianevesdev@gmail.com>
2023-05-02 23:38:11 -0300

    chore: atualization

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import imagemplace from '../src/images/imagemplace.jpg';
 
 function App() {
   return (
     <div className="App">
-<h1>Aninha dev</h1>
+      <h1>Aninha <span>dev</span></h1>
+      <div className='aboutMe'>
+        <img src={imagemplace}/>
+      </div>
     </div>
   );
 }
